797ef92f0dd1104e2144bc229c1ee835494a7d4d
Thalaiyasingam Ajanthan <ajaneng@gmail.com>
2020-03-21 02:44:47 +1100

    1. slideshow has issues --> probably removed

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,5 +1,4 @@
 $("#slideshow > img:gt(0)").hide();
-
 setInterval(function() {
   $('#slideshow > img:first')
     .fadeOut()
@@ -7,4 +6,54 @@ setInterval(function() {
     .fadeIn()
     .end()
     .appendTo('#slideshow');
+}, 3000);
+
+$("#slideshow2 > img:gt(0)").hide();
+setInterval(function() {
+  $('#slideshow2 > img:first')
+    .fadeOut()
+    .next()
+    .fadeIn()
+    .end()
+    .appendTo('#slideshow2');
+}, 3000);
+
+$("#slideshow3 > img:gt(0)").hide();
+setInterval(function() {
+  $('#slideshow3 > img:first')
+    .fadeOut()
+    .next()
+    .fadeIn()
+    .end()
+    .appendTo('#slideshow3');
+}, 3000);
+
+$("#slideshow4 > img:gt(0)").hide();
+setInterval(function() {
+  $('#slideshow4 > img:first')
+    .fadeOut()
+    .next()
+    .fadeIn()
+    .end()
+    .appendTo('#slideshow4');
+}, 3000);
+
+$("#slideshow5 > img:gt(0)").hide();
+setInterval(function() {
+  $('#slideshow5 > img:first')
+    .fadeOut()
+    .next()
+    .fadeIn()
+    .end()
+    .appendTo('#slideshow5');
+}, 3000);
+
+$("#slideshow6 > img:gt(0)").hide();
+setInterval(function() {
+  $('#slideshow6 > img:first')
+    .fadeOut()
+    .next()
+    .fadeIn()
+    .end()
+    .appendTo('#slideshow6');
 }, 3000);
