052537f5ceefa3432c9ccd8a3efc6a6c44539676
Thalaiyasingam Ajanthan <ajaneng@gmail.com>
2020-03-23 02:37:09 +1100

    1. project pages done

--- dgpose/stylesheets/bib.js
+++
@@ -1,9 +0,0 @@
-function showHideBib(event) {
-    event.preventDefault();
-    $(this).next('pre').slideToggle(500);
-}
-
-$(document).ready(function() {
-    $('.bibLink').click(showHideBib);
-});
-
